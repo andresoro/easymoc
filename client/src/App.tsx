bcb18781d742623f20762b83f4856eff6a85a146
Andres Orozco <ao15@my.fsu.edu>
2019-08-27 19:02:14 -0700

    add columns

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import './App.css';
 import { InputBox } from './components/InputBox';
+import { Header } from './components/Header';
 
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      
+      <div className="col1"></div>
+      <div className="col2">
         <div className="header">
-          Hello
-        </div>
-        
-        <div className="content">
-          <InputBox/>
-        </div>
+            <Header/>
+          </div>
+          
+          <div className="content">
+            <InputBox/>
+          </div>
 
-        <div className="footer">
-          <footer>Test</footer>
-        </div>
-      
+          <div className="footer">
+            <footer>Test</footer>
+          </div>
+      </div>
+      <div className="col3"></div> 
     </div>
   );
 }
