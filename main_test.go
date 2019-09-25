29b28ca0d9a6f27c1ad8761b3da46f20c10fcb7b
Andres Orozco <ao15@my.fsu.edu>
2019-09-24 21:32:08 -0700

    simplify test case and add json object body

--- main_test.go
+++ main_test.go
@@ -13,6 +13,16 @@ import (
 	"github.com/andresoro/easymock/server"
 )
 
+var body = `
+	"name":"John",
+	"age":30,
+	"cars": {
+		"car1":"Ford",
+		"car2":"BMW",
+		"car3":"Fiat"
+	}
+`
+
 func TestServer(t *testing.T) {
 	// initialize server with test db
 	t.Log("Starting server test...")
@@ -30,64 +40,6 @@ func TestServer(t *testing.T) {
 		os.RemoveAll("./db_test")
 	}()
 
-	t.Run("testing code 200 creation", func(t *testing.T) {
-		// create our request
-		d := &server.Response{Code: 200, ContentType: "application/json", Body: "test"}
-		body, err := json.Marshal(d)
-		if err != nil {
-			t.Error(err)
-		}
-		req, _ := http.NewRequest(http.MethodPost, "http://localhost:8080/gimme", bytes.NewBuffer(body))
-		req.Header.Set("Content-Type", "application/json")
-
-		client := http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if resp.StatusCode != 200 {
-			t.Errorf("status code on create should be 200 got: %d", resp.StatusCode)
-		}
-
-		// read id from response body
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			t.Fatal("unable to read creation response body")
-		}
-		id := string(bodyBytes)
-
-		// test newly created endpoint
-		url := fmt.Sprintf("http://localhost:8080/r/%s", id)
-
-		req, _ = http.NewRequest(http.MethodGet, url, nil)
-
-		resp, err = client.Do(req)
-		if err != nil {
-			t.Error(err)
-		}
-
-		if resp.StatusCode != 200 {
-			t.Error("not getting proper status code")
-		}
-
-		contentType := strings.Split(resp.Header.Get("Content-Type"), ";")[0]
-
-		if contentType != "application/json" {
-			t.Errorf("incorrect content-type got: %s", contentType)
-		}
-
-		bodyBytes, err = ioutil.ReadAll(resp.Body)
-		if err != nil {
-			t.Error(err)
-		}
-
-		if string(bodyBytes) != d.Body {
-			t.Log(string(bodyBytes))
-			t.Error("body should be the same as the original object")
-		}
-	})
-
 	contentTypes := []string{"application/json", "application/xml", "application/xhtml", "text/json", "text/plain", "text/html", "text/csv"}
 	// this test will run a series of tests for each content type
 	t.Run("testing content types", func(t *testing.T) {
@@ -97,7 +49,7 @@ func TestServer(t *testing.T) {
 
 			t.Run(name, func(t *testing.T) {
 				// create our request
-				d := &server.Response{Code: 200, ContentType: ctype, Body: "test"}
+				d := &server.Response{Code: 200, ContentType: ctype, Body: body}
 				body, err := json.Marshal(d)
 				if err != nil {
 					t.Fatalf("error marshaling payload: %e", err)
