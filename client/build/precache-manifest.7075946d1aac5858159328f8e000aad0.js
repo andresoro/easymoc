b362b7f625254d08f375685b5b531f71961ce07e
Andres Orozco <ao15@my.fsu.edu>
2019-12-29 09:31:35 -0500

    client build

---
+++ client/build/precache-manifest.7075946d1aac5858159328f8e000aad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a364901c41cd10eac518f0fad58a5aba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a90763e246bf7aa9a30",
+    "url": "/static/css/2.7a1db334.chunk.css"
+  },
+  {
+    "revision": "775d4438144f4beb08bb",
+    "url": "/static/css/main.b70059ca.chunk.css"
+  },
+  {
+    "revision": "2a90763e246bf7aa9a30",
+    "url": "/static/js/2.491b910f.chunk.js"
+  },
+  {
+    "revision": "775d4438144f4beb08bb",
+    "url": "/static/js/main.a2facd39.chunk.js"
+  },
+  {
+    "revision": "4810281ade5907b69ff6",
+    "url": "/static/js/runtime~main.e14d06fe.js"
+  }
+]);
